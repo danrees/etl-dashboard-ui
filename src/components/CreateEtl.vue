697a8a50fcd203f1890bf3101dbd1046a17cb9e8
Daniel Rees <daniel.rees@autodata.net>
2017-12-01 17:03:10 -0500

    Remove id from create form, should be set by the service saving

--- src/components/CreateEtl.vue
+++ src/components/CreateEtl.vue
@@ -1,39 +1,34 @@
 <template>
     <div>
-            <div class="field">
-                <div class="control">
-                    <input class="input" placeholder="ID" type="text" v-model="etl.id">
-                </div>
-            </div>
-            <div class="field">
-                <div class="control">
-                    <input class="input" placeholder="Name" type="text" v-model="etl.name">
-                </div>
-            </div>
-            <div class="field">
-                <div class="control">
-                    <input class="input" placeholder="Start routing key" type="text" v-model="etl.startKey">
-                </div>
-            </div>
-            <div class="field">
-                <div class="control">
-                    <input class="input" placeholder="Complete routing key" type="text" v-model="etl.completeKey">
-                </div>
-            </div>
-            <div class="field">
-              <div class="control">
-                <ul>                  
-                  <li v-for="param in etl.parameters" v-bind:key="param">{{ param }}</li>
-                </ul>
-                  <button @click="clearParams">Clear</button>
-              </div>
-            </div>
-            <div class="field">
-                <div class="control">
-                    <input class="input" placeholder="Add Parameter" type="text" v-model="paramToAdd" >
-                    <button @click="addParam">Add</button>
-                </div>
-            </div>
+      <div class="field">
+          <div class="control">
+              <input class="input" placeholder="Name" type="text" v-model="etl.name">
+          </div>
+      </div>
+      <div class="field">
+          <div class="control">
+              <input class="input" placeholder="Start routing key" type="text" v-model="etl.startKey">
+          </div>
+      </div>
+      <div class="field">
+          <div class="control">
+              <input class="input" placeholder="Complete routing key" type="text" v-model="etl.completeKey">
+          </div>
+      </div>
+      <div class="field">
+        <div class="control">
+          <ul>                  
+            <li v-for="param in etl.parameters" v-bind:key="param">{{ param }}</li>
+          </ul>
+            <button @click="clearParams">Clear</button>
+        </div>
+      </div>
+      <div class="field">
+          <div class="control">
+              <input class="input" placeholder="Add Parameter" type="text" v-model="paramToAdd" >
+              <button @click="addParam">Add</button>
+          </div>
+      </div>
     </div>
 </template>
 
